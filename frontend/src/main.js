d64719019161415aa69137d9c3f6f56528d5e1a6
Linus Bittner <2130@htl.rennweg.at>
2026-03-10 11:24:03 +0100

    tailwindcss setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import i18n from './i18n'
+import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'

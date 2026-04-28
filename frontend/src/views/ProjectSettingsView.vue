38ec1c4e906e024ee5535000027c1c025922e7d9
htl3r-2153 <2153@htl.rennweg.at>
2026-04-28 10:29:16 +0200

    typeerror REALLY resolved

--- frontend/src/views/ProjectSettingsView.vue
+++ frontend/src/views/ProjectSettingsView.vue
@@ -2,8 +2,8 @@
     <div class="flex flex-col items-center justify-center">
         <IconLabelInput :iconPath=NameIcon label="Projektnamen ändern" placeholder="Neuen Namen eingeben..." />
         <IconLabelInput :iconPath=DescriptionIcon label="Projektbeschreibung ändern"
-            placeholder="Neue Beschreibung eingeben..." bigField=true />
-            <hr class="w-6/12 dark:border-slate-300 my-5" />
+            placeholder="Neue Beschreibung eingeben..." :bigField="true" />
+        <hr class="w-6/12 dark:border-slate-300 my-5" />
         <IconLabelInput :iconPath=MemberIcon label="Mitglieder hinzufügen" placeholder="Mitglieder finden..." />
     </div>
 </template>

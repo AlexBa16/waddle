5b6368c0865c969ba82543e8d5d7503eb47fdb9a
Samuel Feistl <2133@htl.rennweg.at>
2026-05-05 11:57:05 +0200

    fix darkmode after linus destroyed it :(

--- frontend/src/components/DeleteProject.vue
+++ frontend/src/components/DeleteProject.vue
@@ -2,8 +2,8 @@
     <div class="flex w-1/2 gap-6 px-6 py-5 mt-5 mb-3 bg-[#dde3ef] dark:bg-slate-600 dark:placeholder-indigo-50 dark:border-slate-400 rounded-2xl shadow-md shadow-black/10"
         :class="bigField ? 'flex-col items-stretch' : 'flex-row items-center justify-between'">
         <div class="flex items-center gap-3 shrink-0">
-            <span class="text-sm font-bold text-slate-800 tracking-wide">{{ t('nav.projectSettings.deleteProject.description') }}</span>
-git         </div>
+            <span class="text-sm font-bold text-slate-800 tracking-wide dark:text-orange-50">{{ t('nav.projectSettings.deleteProject.description') }}</span>
+         </div>
         <button class="font-medium text-orange-50 transition-all duration-200 shadow-lg cursor-pointer sm:px-5 lg:px-10 sm:py-4 rounded-xl sm:rounded-2xl sm:text-base lg:text-md bg-red-500 hover:bg-red-600"
             @click="handleDelete">
             {{ t('nav.projectSettings.deleteProject.button') }} 
--- frontend/src/components/IconLabelInput.vue
+++ frontend/src/components/IconLabelInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex w-1/2 gap-6 px-6 py-5 mt-5 mb-3 bg-[#dde3ef] rounded-2xl shadow-md shadow-black/10" :class="bigField
+    <div class="flex w-1/2 gap-6 px-6 py-5 mt-5 mb-3 bg-[#dde3ef] dark:bg-slate-600 rounded-2xl shadow-md shadow-black/10" :class="bigField
             ? 'flex-col items-stretch'
             : 'flex-row items-center justify-between'
         ">
@@ -7,30 +7,25 @@
             <div class="w-5 h-5 flex items-center justify-center text-slate-700 dark:invert">
                 <img :src="iconPath" alt="icon" class="w-full h-full object-contain" />
             </div>
-            <span class="text-sm font-bold text-slate-800 tracking-wide">{{
+            <span class="text-sm font-bold text-slate-800 dark:text-orange-50 tracking-wide">{{
                 label
                 }}</span>
         </div>
 
         <select v-if="selectViewMode" :value="modelValue" @change="emit('update:modelValue', $event.target.value)"
-            class="...">
+            class="... dark:bg-slate-500 dark:text-orange-50 dark:border-slate-400">
             <option value="light">Hell</option>
             <option value="dark">Dunkel</option>
         </select>
 
-        <select v-if="select" :value="modelValue" @change="emit('update:modelValue', $event.target.value)" class="...">
+        <select v-if="select" :value="modelValue" @change="emit('update:modelValue', $event.target.value)" class="... dark:bg-slate-500 dark:text-orange-50 dark:border-slate-400">
             <option value="German">Deutsch</option>
             <option value="English">Englisch</option>
             
         </select>
 
-<<<<<<< HEAD
         <input v-if="!bigField && !select && !selectViewMode" :type="type" :placeholder="placeholder"
-            :value="modelValue" @input="emit('update:modelValue', $event.target.value)" class="..." />
-=======
-        <input v-if="!bigField && !select && !selectViewMode" type="text" :placeholder="placeholder"
-            class="w-6/12 px-4 py-2 text-sm rounded-xl bg-orange-50 border border-indigo-300 text-slate-700 dark:bg-slate-500 dark:placeholder-indigo-50 dark:border-slate-400 placeholder-slate-400 outline-none focus:ring-1 focus:ring-indigo-400 transition-colors duration-150 font-medium" />
->>>>>>> 91d6301 (implement darkmode correctly to settings and project settings)
+            :value="modelValue" @input="emit('update:modelValue', $event.target.value)" class="... dark:bg-slate-500 dark:text-orange-50 dark:border-slate-400" />
 
         <textarea v-else-if="!select && !selectViewMode" :placeholder="placeholder" rows="3"
             class="resize-none w-6/12 px-4 py-2 text-sm rounded-xl bg-orange-50 border border-indigo-300 text-slate-700 dark:bg-slate-500 dark:placeholder-indigo-50 dark:border-slate-400 placeholder-slate-400 outline-none focus:ring-1 focus:ring-indigo-400 transition-colors duration-150 font-medium" />

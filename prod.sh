a1086501d891ead583c394943c45c8948ffba88c
AlexBa16 <contact@alexanderbachinger.com>
2026-08-22 21:47:51 +0200

    Readd Regenerate JWT-Keypair to prod script

--- prod.sh
+++ prod.sh
@@ -7,8 +7,11 @@ docker compose -f compose.yaml -f compose.prod.yaml build --pull
 echo "3. Recreate containers with the new images"
 docker compose --env-file .env.local -f compose.yaml -f compose.prod.yaml up -d --remove-orphans
 
-echo "4. Warm/clear cache"
+echo "4. Regenerate JWT-Keypair"
+docker compose -f compose.yaml -f compose.prod.yaml exec php php bin/console lexik:jwt:generate-keypair --overwrite
+
+echo "5. Warm/clear cache"
 docker compose -f compose.yaml -f compose.prod.yaml exec php bin/console cache:clear --env=prod --no-debug
 
-echo "5. Sanity check"
+echo "6. Sanity check"
 docker compose -f compose.yaml -f compose.prod.yaml exec php bin/console debug:router --env=prod
